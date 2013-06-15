1baf3310ff2f92f1edf62e11a94b433866e93685
santosh singh <santosh.ksingh03@gmail.com>
2013-06-15 17:51:07 +0530

    Initial commit

---
+++ Hello.asm
@@ -0,0 +1,11 @@
+section .text
+
+section .data
+
+global main
+main:
+	mov eax,10
+	add eax,10
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'af72a0da9c4483fa2b53ab5300a51245702e315eba4806af062caec791e2ff69bc4dffe56011946001499d827433fe3ce2c8d0dd24762ef4c4912e4239f541b5'
